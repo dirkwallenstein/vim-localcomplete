8480ae4b9f6449857da22dfa5dc4e0a5a57beba5
Dirk Wallenstein <halsmit@t-online.de>
2013-05-09 18:43:32 +0200

    tests: Add pylibs/thirdparty_tests.py

---
+++ pylibs/thirdparty_tests.py
@@ -0,0 +1,43 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+
+
+import os
+import sys
+import unittest
+
+
+class ThirdpartyTestsError(Exception):
+    """
+    The base exception for this module.
+    """
+
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+import thirdparty
+
+
+class TestPythonToVimStr(unittest.TestCase):
+    def test_me(self):
+        self.assertAlmostEqual(1, 1.0)
+
+    def test_normal_string(self):
+        reprer = thirdparty.PythonToVimStr('without any special cases')
+        self.assertEqual(repr(reprer), r'"without any special cases"')
+
+    def test_embedded_double_quotes(self):
+        reprer = thirdparty.PythonToVimStr('contains \"double\" qoutes')
+        self.assertEqual(repr(reprer), r'"contains \"double\" qoutes"')
+
+    def test_embedded_unicode(self):
+        reprer = thirdparty.PythonToVimStr(u'der \u00fcberfu\u00df')
+        self.assertEqual(repr(reprer), '"der \xc3\xbcberfu\xc3\x9f"')
