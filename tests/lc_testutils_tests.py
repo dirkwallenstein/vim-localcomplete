883cf19dc3b8e68ad80218a34cb2567a4f1b7b22
Dirk Wallenstein <halsmit@t-online.de>
2013-05-13 19:49:55 +0200

    Move system tests into the tests directory

---
+++ tests/lc_testutils_tests.py
@@ -0,0 +1,40 @@
+import mock
+import sys
+import unittest
+
+from tests.lc_testutils import LCTestUtilsError
+from tests.lc_testutils import VimMockFactory
+
+class TestTests(unittest.TestCase):
+    """
+    Test parts of the test framework.
+    """
+
+    def test_mock_object_classes_are_isolated(self):
+        mod_mock = mock.MagicMock()
+        type(mod_mock).test_attribute = "attribute"
+        self.assertEqual(mod_mock.test_attribute, "attribute")
+        self.assertIsInstance(mock.MagicMock().test_attribute, mock.MagicMock)
+
+    def test_unpatched_vim_exception(self):
+        with self.assertRaises(LCTestUtilsError):
+            sys.modules['vim'].command("echo")
+
+    def test_buffer_unset(self):
+        """Raise an exception when no buffer has been specified"""
+        vim_mock = VimMockFactory.get_mock()
+        with self.assertRaises(LCTestUtilsError):
+            return vim_mock.current.buffer
+
+    def test_buffer_full(self):
+        """There is something in the buffer"""
+        vim_mock = VimMockFactory.get_mock(buffer_content=["zero", "one"])
+        self.assertEqual(vim_mock.current.buffer[1], "one")
+
+    def test_last_line_is_line_number(self):
+        vim_mock = VimMockFactory.get_mock(buffer_content=["1", "2", "3"])
+        self.assertEqual(vim_mock.eval("line('$')"), 3)
+
+    def test_current_line_is_line_number(self):
+        vim_mock = VimMockFactory.get_mock(current_line_index=3)
+        self.assertEqual(vim_mock.eval("line('.')"), 4)
---
+++ tests/st_localcomplete_tests.py
@@ -0,0 +1,78 @@
+import contextlib
+import mock
+import unittest
+
+
+# Import Test Utils
+from tests.lc_testutils import VimMockFactory
+from tests.lc_testutils import fix_vim_module
+
+# Import localcomplete
+fix_vim_module()
+from pylibs import localcomplete
+
+
+class SystemTestCompleteLocalMatches(unittest.TestCase):
+
+    @contextlib.contextmanager
+    def _helper_isolate_sut(self,
+            buffer_content,
+            current_line_index,
+            keyword_base,
+            **further_vim_mock_args):
+
+        vim_mock_defaults = dict(
+            above_count=-1,
+            below_count=-1,
+            match_result_order=localcomplete.MATCH_ORDER_CENTERED,
+            want_ignorecase=False,
+            show_origin=False,
+            encoding='utf-8',
+            iskeyword='',
+            keyword_chars='',
+            )
+
+        vim_mock_args = dict(vim_mock_defaults)
+        vim_mock_args.update(further_vim_mock_args)
+
+        produce_mock = mock.Mock(spec_set=[], return_value=[])
+        vim_mock = VimMockFactory.get_mock(
+                buffer_content=buffer_content,
+                current_line_index=current_line_index,
+                keyword_base=keyword_base,
+                **vim_mock_args)
+
+        with mock.patch.multiple(__name__ + '.localcomplete',
+                produce_result_value=produce_mock,
+                vim=vim_mock):
+            yield produce_mock
+
+    def _helper_completion_tests(self,
+            result_list,
+            **isolation_args):
+
+        with self._helper_isolate_sut(**isolation_args
+                ) as produce_mock:
+            localcomplete.complete_local_matches()
+        produce_mock.assert_called_once_with(result_list, mock.ANY)
+
+    def test_system_multiline_matches_of_the_whole_file(self):
+        self._helper_completion_tests(
+                result_list=[u'prize', u'prized', u'priory', u'primary'],
+                buffer_content="priory prize none prized none primary".split(),
+                current_line_index=2,
+                keyword_base='pri')
+
+    def test_system_reverse_above_first_with_limited_range(self):
+        self._helper_completion_tests(
+                result_list=[u'primary', u'prized', u'prize',
+                        u'priest' ,u'primel'],
+                buffer_content=(
+                        "priory prize prized primary "
+                        "primel priest prick".split()),
+                current_line_index=3,
+                keyword_base='pri',
+                above_count=2,
+                below_count=2,
+                match_result_order=(
+                        localcomplete.MATCH_ORDER_REVERSE_ABOVE_FIRST))
